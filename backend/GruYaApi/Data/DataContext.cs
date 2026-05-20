65af794fc50c92144226b64d8659a93dc22e5d6e
NandoAgueroS <nandoaguerojs@gmail.com>
2026-05-20 16:47:51 -0300

    feat: agregar DataContext y filtro global de excepciones

---
+++ backend/GruYaApi/Data/DataContext.cs
@@ -0,0 +1,21 @@
+using GruYaApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConAlambreApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<ServiceRequest> ServiceRequests { get; set; }
+        public DbSet<ProviderProfile> ProviderProfiles { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
--- backend/GruYaApi/Program.cs
+++ backend/GruYaApi/Program.cs
@@ -1,8 +1,13 @@
+using ConAlambreApi.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddControllers(options => options.Filters.AddService<GlobalExceptionFilter>());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
