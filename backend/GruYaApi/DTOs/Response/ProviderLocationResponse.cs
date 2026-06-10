55dbbfabba1c98e0e1838ba76f642282f4c3171c
Nahuel1798 <nahuellucero25@gmail.com>
2026-06-10 11:20:51 -0300

    fix: agrego campo nombre a ProviderLocationResponse

--- backend/GruYaApi/DTOs/Response/ProviderLocationResponse.cs
+++ backend/GruYaApi/DTOs/Response/ProviderLocationResponse.cs
@@ -5,8 +5,8 @@ namespace GruYaApi.DTOs.Response
     public class ProviderLocationResponse
     {
         public int Id { get; set; }
-
         public int UserId { get; set; }
+        public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public ServiceType ServiceType { get; set; }
         public decimal Latitude { get; set; }
