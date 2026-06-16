1e1f53d98f62536d6ff50dd18af7496faeaf1641
NandoAgueroS <nandoaguerojs@gmail.com>
2026-06-16 11:39:50 -0300

    chore: agregar migrations

---
+++ backend/GruYaApi/Migrations/20260616143842_AgregarDistanceEtaMinutesRouteGeometryAAssistance.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GruYaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarDistanceEtaMinutesRouteGeometryAAssistance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "DistanceKm",
+                table: "Assistances",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "EtaMinutes",
+                table: "Assistances",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RouteGeometry",
+                table: "Assistances",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DistanceKm",
+                table: "Assistances");
+
+            migrationBuilder.DropColumn(
+                name: "EtaMinutes",
+                table: "Assistances");
+
+            migrationBuilder.DropColumn(
+                name: "RouteGeometry",
+                table: "Assistances");
+        }
+    }
+}
